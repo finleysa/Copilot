a44f034d8e85cca847cb2c5de759ed4d5bf0d7a4
Steven Finley <steve.a.finley@gmail.com>
2014-06-13 15:49:10 -0500

    facebook login possibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 TravelBuddy::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+
   root :to => "static#home"
 
   get "trip/new"
