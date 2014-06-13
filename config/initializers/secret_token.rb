37e48d2329b167c801d4e6c4fe1b857da69069cc
Steven Finley <steve.a.finley@gmail.com>
2014-06-12 23:13:47 -0500

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TravelBuddy::Application.config.secret_key_base = 'e0c2a343274e3e880e87dca8c25eb5133c96dad7ba2b37531893886c3f8f7a7c286349c58b7aaca6b79104f6bcddf6e6c32eeb08122632952577a5f51e780d8b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TravelBuddy::Application.config.session_store :cookie_store, key: '_Travel_Buddy_session'
