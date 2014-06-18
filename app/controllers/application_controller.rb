020cad1cead987733194d19cf5fd6173504385ee
Steven Finley <steve.a.finley@gmail.com>
2014-06-18 14:29:24 -0500

    finished edit page. still needs a new image form somewhere... profile page?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:image, :username]
+    devise_parameter_sanitizer.for(:account_update) << [:image, :username]
   end
 end
