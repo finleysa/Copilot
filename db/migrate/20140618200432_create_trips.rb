484c798269226c8ed91b248c1fa8a7b749580c33
Steven Finley <steve.a.finley@gmail.com>
2014-06-19 18:25:28 -0500

    switched trips migration back to normal

--- db/migrate/20140618200432_create_trips.rb
+++ db/migrate/20140618200432_create_trips.rb
@@ -2,13 +2,14 @@
 class CreateTrips < ActiveRecord::Migration
   def change
     create_table :trips do |t|
-      t.string :starting_lat
-      t.string :starting_lng
-      t.string :ending_lat
-      t.string :ending_lng
+      t.string :lat_dest
+      t.string :lng_dest
+      t.string :lat_local
+      t.string :lng:local
       t.datetime :date
 
       t.timestamps
+
     end
     add_index("users", "users_id")
   end
