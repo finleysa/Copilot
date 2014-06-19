bf251d31fa27f28fcce0e6b777a3c5a0f5a7b9b0
Steven Finley <steve.a.finley@gmail.com>
2014-06-19 10:38:18 -0500

    trip migration edited

--- db/migrate/20140618200432_create_trips.rb
+++ db/migrate/20140618200432_create_trips.rb
@@ -1,8 +1,15 @@
+
 class CreateTrips < ActiveRecord::Migration
   def change
     create_table :trips do |t|
+      t.string :starting_lat
+      t.string :starting_lng
+      t.string :ending_lat
+      t.string :ending_lng
+      t.datetime :date
 
       t.timestamps
     end
+    add_index("users", "users_id")
   end
 end
