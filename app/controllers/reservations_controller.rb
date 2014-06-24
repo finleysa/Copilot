be309dca1bac090d95bf81d518512a92c1fb45d4
Steven Finley <steve.a.finley@gmail.com>
2014-06-24 15:29:55 -0500

    reservations model create method updated

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,3 +1,9 @@
 class ReservationsController < ApplicationController
   before_action :authenticat_user!
+
+  def create
+    @reservation = current_user.reservations.new(reservation_params)
+    if @reservation.save
+      redirect_to current_user
+  end
 end
